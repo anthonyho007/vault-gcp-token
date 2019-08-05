f0c242f738b867e864f72abd4596ed8bba80edf1
Anthony Ho <anthony.ho@shopify.com>
2019-08-04 20:04:47 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vault Plugin: Vault GCP Token
+
+A vault plugin for requesting short-lived GCP service account token
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+
+	log "github.com/golang/glog"
+	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/sdk/plugin"
+)
+
+func main() {
+	apiClientMeta := &api.PluginAPIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	flags.Parse(os.Args[1:])
+
+	tlsConfig := apiClientMeta.GetTLSConfig()
+	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
+
+	if err := plugin.Serve(&plugin.ServeOpts{
+		BackendFactoryFunc: nil, // put the backend here
+		TLSProviderFunc:    tlsProviderFunc,
+	}); err != nil {
+		log.Error("failed to initialize vault-gcp-token plugin")
+		os.Exit(1)
+	}
+}
